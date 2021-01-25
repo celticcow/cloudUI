2b0f8f885d47755fac24b9fa50805296b2588158
Celticcow <gregory.dunlap@gmail.com>
2021-01-25 12:10:42 -0600

    read both groups ... know what need where

--- group_sync.py
+++ group_sync.py
@@ -19,6 +19,23 @@ def login_api(key, mds, domain):
     return response["sid"]
 #end of api
 
+"""
+take 2 lists, send you the difference between the 2
+"""
+def ListDiff(li1, li2):
+    return(list(list(set(li1)-set(li2)) + list(set(li2)-set(li1))))
+
+"""
+List1 has things List2 doens't
+"""
+def ListDiff1(li1, li2):
+    return(list(set(li1)-set(li2)))
+"""
+List2 has things List1 doesn't
+"""
+def ListDiff2(li1, li2):
+    return(list(set(li2)-set(li1)))
+
 def build_group_list():
     group_list = list()
     debug = 0
@@ -71,6 +88,9 @@ def check_cma(mds, cma, grp_list):
         print(grp, end="**\n")
         if(apifunctions.group_exist(mds, grp, sid)):
             print("exist")
+
+            cma_group_contents_list = list()
+
             grp_json = {
                 "name" : grp
             }
@@ -79,12 +99,96 @@ def check_cma(mds, cma, grp_list):
             print(json.dumps(get_grp_contents_json))
             print("******")
             print(get_grp_contents_json['members'])
+
+            tmp1 = get_grp_contents_json['members']
+            #print(type(tmp1))
+
+            for mem in get_grp_contents_json['members']:
+                #print(mem)
+                print("_____________________")
+                #print(mem['type'])
+
+                if(mem['type'] == "host"):
+                    print(mem['ipv4-address'])
+                    cma_group_contents_list.append(mem['ipv4-address'])
+                elif(mem['type'] == "network"):
+                    print(mem['subnet4'])
+                    print(mem['subnet-mask'])
+                    print(mem['mask-length4'])
+
+                    toadd = mem['subnet4'] + "/" + str(mem['mask-length4'])
+                    cma_group_contents_list.append(toadd)
+
+                elif(mem['type'] == "address-range"):
+                    print(mem['ipv4-address-first'])
+                    print(mem['ipv4-address-last'])
+
+                    toadd = mem['ipv4-address-first'] + "-" + mem['ipv4-address-last']
+                    cma_group_contents_list.append(toadd)
+                else:
+                    #something went wrong here.
+                    pass
+
+            #grp_mem_len = len(get_grp_contents_json['members']
+            #for i in len(get_grp_contents_json['members']):
+                #print(get_grp_contents_json['members'][i])
+
+            ## get flask group contents : use grp_json
+            flask_grp_members = {}
+
+            url = "http://localhost:5000/group_content"
+
+            headers = {"Accept" : "application/json"}
+
+            #response = requests.get(url, headers=headers, params=grp_json)
+            response = requests.post(url, headers=headers, json=grp_json)
+            if(response.status_code == 200):
+                ##got a good reply
+                flask_grp_members = response.json()
+                print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+                print(flask_grp_members)
+                print(flask_grp_members['ranges'])
+
+                json_file_list = flask_grp_members['ranges']
+                print("@@@@@@@")
+                print(type(json_file_list))
+
+                ####
+                # need to compare 
+
+                print("++++++++++++++++++++++++++++")
+                print(cma_group_contents_list)
+                print(json_file_list)
+
+                cma_group_contents_list.sort()
+                json_file_list.sort()
+
+
+                file_need  = ListDiff1(cma_group_contents_list, json_file_list)
+                cma_need   = ListDiff2(cma_group_contents_list, json_file_list)
+
+                print("CMA Need :")
+                print(cma_need)
+                print("File Need :")
+                print(file_need)
+                print("++++++++++++++++++++++++++++")
+            
+            else:
+                pass
+
+
             """
             need to sort, analyze here
             """
+        else:
+            #group does not exist on cma
+            pass
+    #end of for loop
+            
 
     time.sleep(5)
     apifunctions.api_call(mds, "logout", {}, sid)
+#end of check cma
 
 def main():
     print("")
---
+++ junk_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+def ListDiff(li1, li2):
+    return(list(list(set(li1)-set(li2)) + list(set(li2)-set(li1))))
+
+def ListDiff1(li1, li2):
+    return(list(set(li1)-set(li2)))
+
+
+def ListDiff2(li1, li2):
+    return(list(set(li2)-set(li1)))
+
+
+def main():
+    print("testing only")
+
+    list1 = ["192.168.50.2", "192.168.20.0/24", "192.168.1.4-192.168.1.99"]
+    list2 = ["192.168.50.2", "192.168.1.4-192.168.1.99", "192.168.20.0/24"]
+    list3 = ["192.168.50.2"]
+
+    print(type(list1))
+    print(list1)
+    print("S P A C E")
+    print(list2)
+
+    list1.sort()
+    list2.sort()
+
+    print("-------------------------------")
+    print(list1)
+    print(list2)
+
+    if(list1 == list2):
+        print("equal")
+    else:
+        print("NOT equal")
+    
+    print(ListDiff(list1, list2))
+    print(ListDiff(list1, list3))
+    print(ListDiff(list3, list1))
+
+    print(ListDiff1(list1, list3))
+    print(ListDiff1(list3, list1))
+
+
+    print(ListDiff2(list1, list3))
+    print(ListDiff2(list3, list1))
+
+
+if __name__ == "__main__":
+    main()   
